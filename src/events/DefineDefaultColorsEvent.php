fcbb5c7d7ab3e5c0dc04ac2c2ebeb86eeae1a940
Marko Gajic <marko.gajic@developion.com>
2022-05-22 20:05:25 +0200

    button field v1

---
+++ src/events/DefineDefaultColorsEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Developion\Core\events;
+
+use yii\base\Event;
+
+class DefineDefaultColorsEvent extends Event
+{
+	public ?string $defaultTextColor = null;
+	public ?string $defaultTextHoverColor = null;
+	public ?string $defaultBackgroundColor = null;
+	public ?string $defaultBackgroundHoverColor = null;
+}
