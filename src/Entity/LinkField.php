cc249ae66ffcc6bbb53f8a1ea7a3a625ace39edc
Marko Gajic <marko.gajic@developion.com>
2022-05-29 03:14:31 +0200

    Link Field finished

---
+++ src/Entity/LinkField.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Developion\Core\Entity;
+
+use craft\elements\db\ElementQuery;
+use Spatie\DataTransferObject\DataTransferObject;
+
+class LinkField extends DataTransferObject
+{
+	public ?string $text;
+	public ?string $linkType;
+	public ?bool $target;
+	public ElementQuery $entry;
+	public ElementQuery $asset;
+	public ?string $url;
+	public ?string $phone;
+	public ?string $email;
+
+	public function getUrl(...$args): string
+	{
+		switch ($this->linkType) {
+			case 'entry': return $this->entry->one()?->url;
+			case 'asset': return $this->asset->one()?->getUrl();
+			case 'phone': return "tel:{$this->phone}";
+			case 'email': return "mailto:{$this->email}";
+			case 'url': return $this->url;
+		}
+		return '';
+	}
+
+	public function toArray(): array
+	{
+		$array = parent::toArray();
+
+		$array['entry'] = $array['entry']->one()?->id;
+		$array['asset'] = $array['asset']->one()?->id;
+
+		return $array;
+	}
+}
--- src/fields/Link.php
+++ src/fields/Link.php
@@ -3,19 +3,22 @@
 namespace Developion\Core\fields;
 
 use Craft;
-use craft\base\EagerLoadingFieldInterface;
-use craft\base\Element;
 use craft\base\ElementInterface;
 use craft\base\Field;
-use craft\helpers\ArrayHelper;
+use craft\elements\Asset;
+use craft\elements\Entry;
+use craft\helpers\Json;
 use craft\validators\ArrayValidator;
+use Developion\Core\Entity\LinkField;
 use yii\db\Schema;
 
-class Link extends Field implements EagerLoadingFieldInterface
+class Link extends Field
 {
 	public array $allowedLinkTypes = [];
 
 	public bool $textNotOptional = false;
+
+	private string $_errorMessage = 'The field couldn\'t be saved.';
 
 	public static function displayName(): string
 	{
@@ -35,7 +38,7 @@ class Link extends Field implements EagerLoadingFieldInterface
 		$rules = parent::defineRules();
 		$rules[] = [
 			['allowedLinkTypes'],
-			ArrayValidator::class,
+			ArrayValidator::class ,
 			'min' => 1,
 			'tooFew' => Craft::t('developion-core', 'You must select at least {min, number} of the {attribute}.'),
 			'skipOnEmpty' => false
@@ -43,62 +46,57 @@ class Link extends Field implements EagerLoadingFieldInterface
 		return $rules;
 	}
 
-	public function getContentColumnType(): array|string
+	public static function valueType(): string
 	{
-		$contentColumnTypes = [
-			'text' => SCHEMA::TYPE_STRING,
-			'linkType' => SCHEMA::TYPE_STRING,
-			'target' => SCHEMA::TYPE_BOOLEAN,
-		];
+		return LinkField::class;
+	}
 
-		if ($this->allowedLinkTypes === ['*']) {
-			$allTypes = array_map(
-				fn ($type) => $type['value'],
-				$this->getAvailableLinkTypes()
-			);
-			foreach ($allTypes as $linkType) {
-				$contentColumnTypes[$linkType] = SCHEMA::TYPE_STRING;
-			}
-
-			return $contentColumnTypes;
-		}
-
-		foreach ($this->allowedLinkTypes as $linkType) {
-			$contentColumnTypes[$linkType] = SCHEMA::TYPE_STRING;
-		}
-
-		return $contentColumnTypes;
+	public function getContentColumnType(): array |string
+	{
+		return SCHEMA::TYPE_TEXT;
 	}
 
 	public function normalizeValue(mixed $value, ?ElementInterface $element = null): mixed
 	{
-		if (!$value) {
-			$value = ArrayHelper::map(
-				array_keys($this->getContentColumnType()),
-				fn ($type) => $type,
-				function ($type) {
-					if ($type == 'target') {
-						return false;
-					}
-					return '';
-				}
-			);
+		if ($value instanceof LinkField) {
+			return $value;
 		}
 
-		if (array_key_exists('asset', $value) && gettype($value['asset']) == 'string') {
-			$value['asset'] = json_decode($value['asset']);
+		if (is_string($value) && !empty($value)) {
+			$value = Json::decodeIfJson($value);
 		}
 
-		if (array_key_exists('entry', $value) && gettype($value['entry']) == 'string') {
-			$value['entry'] = json_decode($value['entry']);
+		if (!is_array($value)) {
+			$value = $this->_default();
 		}
 
+		if (empty($value['linkType'])) {
+			$value['linkType'] = $this->getAllowedLinkTypes()[0]['value'];
+		}
+
+		$selectedValue = $value[$value['linkType']];
+		foreach ($this->getAllowedLinkTypes() as $type) {
+			$value[$type['value']] = $this->_default()[$type['value']];
+		}
+		$value[$value['linkType']] = is_string($value[$value['linkType']]) ? $selectedValue : (array)$selectedValue;
+
+		$value['entry'] = Entry::find()->id(reset($value['entry']));
+		$value['asset'] = Asset::find()->id(reset($value['asset']));
+
+		return new LinkField($value);
+	}
+
+	public function serializeValue(mixed $value, ?ElementInterface $element = null): mixed
+	{
+		if ($value instanceof LinkField) {
+			return $value->toArray();
+		}
 		return $value;
 	}
 
 	protected function inputHtml(mixed $value, ElementInterface $element = null): string
 	{
-		return Craft::$app->getView()->renderTemplate("developion-core/_fields/link/input", [
+		return Craft::$app->getView()->renderTemplate('developion-core/_fields/link/input', [
 			'value' => $value,
 			'field' => $this,
 		]);
@@ -106,7 +104,7 @@ class Link extends Field implements EagerLoadingFieldInterface
 
 	public function getSettingsHtml(): ?string
 	{
-		return Craft::$app->getView()->renderTemplate("developion-core/_fields/link/settings", [
+		return Craft::$app->getView()->renderTemplate('developion-core/_fields/link/settings', [
 			'field' => $this,
 		]);
 	}
@@ -118,34 +116,32 @@ class Link extends Field implements EagerLoadingFieldInterface
 
 	public function validateFieldStructure(ElementInterface $element): void
 	{
-		/** @var Element $element */
+		/** @var LinkField $value */
 		$value = $element->getFieldValue($this->handle);
 
-		if (empty($value['text']) && $this->textNotOptional) {
-			$element->addError("$this->handle", Craft::t('developion-core', 'The button text field can\'t be empty.'));
+		if (empty($value->text) && $this->textNotOptional) {
+			$this->addError('text', $this->_getErrors('text'));
+		}
+		if ($value->linkType == 'entry' && !$value->entry->one()) {
+			$this->addError('entry', $this->_getErrors('entry'));
+		}
+		if ($value->linkType == 'asset' && !$value->asset->one()) {
+			$this->addError('asset', $this->_getErrors('asset'));
+		}
+		if ($value->linkType == 'url' && empty($value->url)) {
+			$this->addError('url', $this->_getErrors('url'));
+		}
+		if ($value->linkType == 'email' && empty($value->email)) {
+			$this->addError('email', $this->_getErrors('email'));
+		}
+		if ($value->linkType == 'phone' && empty($value->phone)) {
+			$this->addError('phone', $this->_getErrors('phone'));
 		}
 
-		if ($value['linkType'] == 'entry' && empty($value['entry'])) {
-			$element->addError($this->handle, Craft::t('developion-core', 'Entry can\'t be empty if the link type is Entry.'));
+		if ($this->hasErrors()) {
+			$element->addError($this->handle, $this->_errorMessage);
+			$element->addModelErrors($this, $this->handle);
 		}
-		if ($value['linkType'] == 'asset' && empty($value['asset'])) {
-			$element->addError($this->handle, Craft::t('developion-core', 'Asset can\'t be empty if the link type is Asset.'));
-		}
-		if ($value['linkType'] == 'url' && empty($value['url'])) {
-			$element->addError($this->handle, Craft::t('developion-core', 'Url can\'t be empty if the link type is Url.'));
-		}
-		if ($value['linkType'] == 'email' && empty($value['email'])) {
-			$element->addError($this->handle, Craft::t('developion-core', 'Email can\'t be empty if the link type is Email.'));
-		}
-		if ($value['linkType'] == 'phone' && empty($value['phone'])) {
-			$element->addError($this->handle, Craft::t('developion-core', 'Phone can\'t be empty if the link type is Phone.'));
-		}
-	}
-
-	public function getEagerLoadingMap(array $sourceElements): array|null|false
-	{
-		// dd($sourceElements);
-		return [];
 	}
 
 	public function getAvailableLinkTypes(): array
@@ -165,9 +161,37 @@ class Link extends Field implements EagerLoadingFieldInterface
 			return $this->getAvailableLinkTypes();
 		}
 
-		return array_filter(
-			$this->getAvailableLinkTypes(),
-			fn ($type) => in_array($type['value'], $this->allowedLinkTypes)
-		);
+		return array_filter($this->getAvailableLinkTypes(), fn($type) => in_array($type['value'], $this->allowedLinkTypes));
+	}
+
+	private function _getErrors(string $attribute): string
+	{
+		$errors = [
+			'text' => Craft::t('developion-core', 'The link text field can\'t be empty.'),
+			'entry' => Craft::t('developion-core', 'Entry can\'t be empty if the link type is Entry.'),
+			'asset' => Craft::t('developion-core', 'Asset can\'t be empty if the link type is Asset.'),
+			'url' => Craft::t('developion-core', 'Url can\'t be empty if the link type is Url.'),
+			'email' => Craft::t('developion-core', 'Email can\'t be empty if the link type is Email.'),
+			'phone' => Craft::t('developion-core', 'Phone can\'t be empty if the link type is Phone.'),
+		];
+
+		if (!array_key_exists($attribute, $errors))
+			return '';
+
+		return $errors[$attribute];
+	}
+
+	private function _default(): array
+	{
+		return [
+			'text' => '',
+			'target' => false,
+			'linkType' => '',
+			'entry' => [0],
+			'asset' => [0],
+			'url' => '',
+			'phone' => '',
+			'email' => '',
+		];
 	}
 }
