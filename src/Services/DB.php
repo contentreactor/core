d7837908831177ff711fda105fd0c8d21ed1681f
Marko Gajić <marko.gajic@developion.com>
2022-07-07 11:05:06 +0200

    error logging

--- src/Services/DB.php
+++ src/Services/DB.php
@@ -80,6 +80,7 @@ class DB
 			}
 			$transaction->commit();
 		} catch (\Throwable $th) {
+			Craft::debug($th->getTrace(), 'developion-core');
 			$transaction->rollBack();
 			return false;
 		}
@@ -139,6 +140,7 @@ class DB
 			$setting->save();
 			$transaction->commit();
 		} catch (\Throwable $th) {
+			Craft::debug($th->getTrace(), 'developion-core');
 			$transaction->rollBack();
 			return false;
 		}
